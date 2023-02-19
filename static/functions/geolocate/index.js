2d3a30a205010bc2c0671f1ea13f3bdc8c424905
Ardie Wen <ardie.wen@hunterlily.co>
2023-02-19 09:18:57 -0500

    chore: test geolocate function

---
+++ static/functions/geolocate/index.js
@@ -0,0 +1,47 @@
+const Reader = require("@maxmind/geoip2-node").Reader;
+
+const fs = require("fs");
+const path = require("path");
+
+// fs.readFile(resolved, (err, results) => console.log(results))
+
+module.exports.handler = async (event, context) => {
+  const rawIp = event.headers["x-forwarded-for"];
+
+  const [clientIp, ...proxyIp] = rawIp.split(",");
+
+  console.log("[Geolocate] Requested Client IP", clientIp);
+  console.log("Lambda task root: ", process.env.LAMBDA_TASK_ROOT);
+  const fileName = "./data/GeoLite2-Country.mmdb";
+  const resolved =
+    process.env.LAMBDA_TASK_ROOT === "/var/task"
+      ? path.resolve(
+          process.env.LAMBDA_TASK_ROOT,
+          "./src/static/functions/geolocate/",
+          fileName
+        )
+      : path.resolve(__dirname, fileName);
+  console.log("Resolved", resolved);
+
+  const reader = await Reader.open(resolved);
+
+  if (clientIp === "::1" || clientIp === "::ffff:127.0.0.1") {
+    console.log("[Geolocate] Warning: localhost request");
+
+    const { country } = await reader.country("138.197.148.82");
+
+    console.log(country);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ country }),
+    };
+  }
+
+  const { country } = await reader.country(clientIp);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ country }),
+  };
+};
